212c47ee321f7d5591fe1898c1d3b0477bd6e2d3
Parvaneh <Amirparvaneh138@gmail.com>
2021-05-17 12:38:12 +0430

    consuming rest api of countries before testing our getCardOwner service.

--- src/main/java/com/rest/open/openbanking/controller/CardController.java
+++ src/main/java/com/rest/open/openbanking/controller/CardController.java
@@ -1,15 +1,25 @@
 package com.rest.open.openbanking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 
 @RestController
-@RequestMapping(path = "/")
 public class CardController {
 
-    @GetMapping(path = "/getOwnerOfCard")
-    public String getId(){
-        return "this is the information of this owner";
-    }
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static String url = "https://restcountries.eu/rest/v2/all";
+    @GetMapping(path = "/getC")
+    public List<Object> getId(){
+        Object[] countries = restTemplate.getForObject(url,Object[].class);
+        return Arrays.asList(countries);
+     }
 }
