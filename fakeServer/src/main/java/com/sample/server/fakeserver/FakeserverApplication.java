649e188a6f63f31475e0e3a50b1508336b4fcdbd
Parvaneh <Amirparvaneh138@gmail.com>
2021-05-18 09:31:48 +0430

    creating the server

---
+++ fakeServer/src/main/java/com/sample/server/fakeserver/FakeserverApplication.java
@@ -0,0 +1,13 @@
+package com.sample.server.fakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeserverApplication.class, args);
+    }
+
+}
---
+++ fakeServer/src/test/java/com/sample/server/fakeserver/FakeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.server.fakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
