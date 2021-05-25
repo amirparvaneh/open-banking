5b8a3ca8701cb16be162985c80471d445a6d2aca
Parvaneh <Amirparvaneh138@gmail.com>
2021-05-25 16:00:58 +0430

    card service

--- fakeServer/src/main/java/com/sample/server/fakeserver/repository/CardRep.java
+++
@@ -1,8 +0,0 @@
-package com.sample.server.fakeserver.repository;
-
-import com.sample.server.fakeserver.entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CardRep extends JpaRepository<Card,String>  {
- }
